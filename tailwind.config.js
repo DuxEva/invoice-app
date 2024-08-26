4a821b6136c33a8dc6eb81f6a52b5fb7b605c2fa
DuvEva <evariste.dusingizimana@amalitechtraining.org>
2024-08-26 12:58:28 +0200

    generate angular project set up tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          1: "#7C5DFA",
+          2: "#9277FF",
+          3: "#7E88C3",
+        },
+        darkblue: {
+          1: "#1E2139",
+          2: "#252945",
+        },
+        grey: {
+          1: "#F8F8FB ",
+          2: "#DFE3FA",
+          3: "#888EB0",
+        },
+        black: {
+          1: "#0C0E16",
+          2: "#141625",
+        },
+        red: {
+          1: "#EC5757",
+          2: "#9277FF",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
