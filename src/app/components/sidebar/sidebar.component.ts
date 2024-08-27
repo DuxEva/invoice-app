8c31ecaf0a3ee57f186f38a49f9d5da1fce94453
DuvEva <evariste.dusingizimana@amalitechtraining.org>
2024-08-27 10:30:22 +0200

    fix dark mode change

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,47 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
-  @Output() theme = new EventEmitter();
-  @Input() isDarkMode = true;
+export class SidebarComponent implements OnInit {
+  darkMode = false;
 
-  toggleTheme() {
-    this.theme.emit();
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.applyThemePreference();
+    }
+  }
+
+  applyThemePreference() {
+    const savedTheme = localStorage.getItem('theme');
+    if (
+      savedTheme === 'dark' ||
+      (!savedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      this.darkMode = true;
+      this.updateDarkModeClass(true);
+    } else {
+      this.darkMode = false;
+      this.updateDarkModeClass(false);
+    }
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    localStorage.setItem('theme', this.darkMode ? 'dark' : 'light');
+    this.updateDarkModeClass(this.darkMode);
+  }
+
+  private updateDarkModeClass(isDarkMode: boolean) {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   }
 }
